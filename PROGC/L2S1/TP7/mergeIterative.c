ea0024952789a304b7a9d58a5b20a09e861ed3a4
TwilCynder <twilcynder@gmail.com>
2019-11-25 11:09:40 +0100

    tp algo 8-1

--- PROGC/L2S1/TP7/mergeIterative.c
+++ PROGC/L2S1/TP7/mergeIterative.c
@@ -1,16 +1,44 @@
 #include "pile.h"
 #include "mergeSort.h"
 
-void mergeSortRecursive(int T[], int size, int M[]){
-    pile_t p;
-    pile_t q;
-    int mid;
+void mergeSortIterative(int T[], int size, int M[]){
+    pile_t p; init_pile(&p);
+    pile_t q; init_pile(&q);
+    int f1, l1, f2, l2;
 
-    int first = 0;
-    int last = size - 1;
+    for (int i = 0; i < size; i++){
+      empiler(&p, i);
+      empiler(&p, i);
+    }
 
-    while (){
+    while (p.som > 1){ //si p ne contient plus que 2 éléments, on a fini
+      while(!pile_vide(&p)){
+        l2 = sommet(&p); depiler(&p);
+        f2 = sommet(&p); depiler(&p);
 
+        if (!pile_vide(&p)){ // /!\ les 2 sous tableaux doivent se suivre
+          l1 = sommet(&p); depiler(&p);
+          f1 = sommet(&p); depiler(&p);
+          merge(T, f1, l1, f2, l2, M);
+          f2 = f1;
+        }
+        empiler(&q, l2);
+        empiler(&q, f2); //on empile first après last pour que tout soit à l'envers
+      }
+
+      init_pile(&p); //on vide p
+
+      while (!pile_vide(&q)){
+        empiler(&p, sommet(&q)) ; depiler(&q);
+        empiler(&p, sommet(&q)) ; depiler(&q);
+      }
+
+      init_pile(&q); //on vide q
 
     }
+}
+
+int main(int argc, char const *argv[]) {
+  test_S(&mergeSortIterative);
+  return 0;
 }
