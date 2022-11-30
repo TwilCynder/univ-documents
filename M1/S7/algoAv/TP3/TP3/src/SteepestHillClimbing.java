2edb5b910f27b48e741a816d1c2eef1e2b161f85
TwilCynder <twilcynder@gmail.com>
2022-11-30 03:47:43 +0100

    merge

--- M1/S7/algoAv/TP3/TP3/src/SteepestHillClimbing.java
+++ M1/S7/algoAv/TP3/TP3/src/SteepestHillClimbing.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -12,13 +16,26 @@ public class SteepestHillClimbing {
     }
   }
 
+  public class InvalidInputFileException extends Exception {
+    InvalidInputFileException(String message){
+      super("Contenu de fichier d'entrée invalide " + message);
+    }
+  }
+
   //NO DIMENSION CHECK
+  /**
+   * Renvoie la valeur d'une solution
+   * @param solution représentée par un vecteur de booléens  
+   * @param matrix d'ints
+   * @return valeur
+   */
   public int valeur_solution(boolean[] sol, int[][] matrix){
     int size = sol.length;
 
     int res = 0;
     for (int i = 0; i < size; i++){
       if (sol[i]){
+        //note : je fais ici une multiplication matricielle entre la ligne et mon vecteur
         for (int j = 0; j < size; j++){
           if (sol[j]) res+= matrix[i][j];
         }
@@ -32,13 +49,14 @@ public class SteepestHillClimbing {
 
 
   /**
-   * Retourne la nouvelle valeur si un changement a été fait, 0 sinon  
-   * //a optimiser
-   * //NO DIMENSION CHECK
-   * @param sol
-   * @param valeur_solution n'existe que pour ne pas avoir à calculer 2 fois la valeur de la solution initiale
-   * @param matrix
-   * @return
+   * Pour une solution donnée, teste les déplacements possibles et garde le meilleur 
+   * (en modifiant la solution passée, afin d'éviter les constructions inutiles).  
+   * Renvoie la nouvelle valeur, OU 0 si aucun changement n'a été fait. 
+   * //a optimiser, supprimer temp_sol  
+   * @param solution de départ, qui pourra être modifiée.
+   * @param valeur_solution : valeur de la solution passée, passée en paramètre pour éviter de la recalculer pour cette fonction alors qu'on l'a déjà calculée avant. 
+   * @param matrix d'ints 
+   * @return Nouvelle valeur de la solution, ou zéro si solution était un maximum local.  
    */
   private int meilleur_voisin(boolean[] sol, int valeur_solution, int[][] matrix){
     int size = sol.length;
@@ -77,7 +95,8 @@ public class SteepestHillClimbing {
   }
 
   /**
-   * Utile dans le cas où on veut le meilleur_voisin d'une sol dont on n'a pas encore la valeur
+   * Identique à meilleur_voisin(boolean[], int, int[][]), mais calcule elle-même la valeur de la solution donnée, 
+   * utile donc si on n'a pas encore calculé ladite valeur.  
    * @param sol
    * @param matrix
    * @return
@@ -86,6 +105,14 @@ public class SteepestHillClimbing {
     return meilleur_voisin(sol, valeur_solution(sol, matrix), matrix);
   }
 
+  /**
+   * Applique l'algo de Steepest Hill Climbing à partir d'une solution donnée. 
+   * Modifie la solution en paramètre (éviter les copies) et renvoie la nouvelle valeur. 
+   * @param matrix d'ints. 
+   * @param initial solution initiale
+   * @return valeur de la solution finale trouvée par l'algo (maximum local)
+   * @throws DimensionMismatchException si for some reason la matrice et la solution n'ont pas la même dimension.
+   */
   public int climb(int[][] matrix, boolean[] initial) throws DimensionMismatchException {
     int size = initial.length;
     if (matrix.length != size) throw new DimensionMismatchException();
@@ -100,6 +127,16 @@ public class SteepestHillClimbing {
     return meilleureVal;
   }
 
+
+  /**
+   * Applique l'algo de Steepest Hill Climbing à partir d'une solution donnée, en s'arrêtant à au plus max_depls changements.  
+   * Modifie la solution en paramètre (éviter les copies) et renvoie la nouvelle valeur. 
+   * @param matrix d'ints. 
+   * @param initial solution initiale
+   * @param max_depls nombre maximal de changements/déplacements effectués avant de s'arrêter. 
+   * @return valeur de la solution trouvée (potentiel minimum local, si max_depl pas atteint)
+   * @throws DimensionMismatchException
+   */
   public int climb(int[][] matrix, boolean[] initial, int max_depls) throws DimensionMismatchException {
     int size = initial.length;
     if (matrix.length != size) throw new DimensionMismatchException();
@@ -113,6 +150,10 @@ public class SteepestHillClimbing {
     return meilleureVal;
   }
 
+  /**
+   * Solution, avec sa valeur (qui peut être déduite de la solution, mais on préfère la garder dans cette structure
+   * une fois qu'on l'a calculée pour limiter au maximum le nombre de calculs de valeurs)
+   */
   public class Result {
     public boolean[] vecteur;
     public int valeur;
@@ -120,9 +161,17 @@ public class SteepestHillClimbing {
       this.vecteur = vecteur;
       this.valeur = valeur;
     }
-    
   }
 
+  /**
+   * Applique l'algo de Steepest Hill Climbing à partir d'une solution aléatoire, en s'arrêtant à au plus max_depls changements.  
+   * Modifie la solution en paramètre (éviter les copies) et renvoie la nouvelle valeur. 
+   * @param matrix d'ints. 
+   * @param initial solution initiale
+   * @param max_depls nombre maximal de changements/déplacements effectués avant de s'arrêter. 
+   * @return Result la solution avec sa valeur déjà calculée
+   * @throws DimensionMismatchException
+   */
   public Result climb(int[][] matrix, int max_depls) throws DimensionMismatchException{
     int size = matrix.length;
 
@@ -136,18 +185,22 @@ public class SteepestHillClimbing {
     return new Result(sol, val);
   }
 
+  /**
+   * Applique l'algo de Steepest Hill Climbing à partir d'une solution aléatoire.  
+   * @param matrix d'ints.
+   * @return
+   * @throws DimensionMismatchException
+   */
   public Result climb(int[][] matrix) throws DimensionMismatchException{
     return climb(matrix, 0);
   }
 
   public Result climb_restart(int[][] matrix, int max_essais) throws DimensionMismatchException {
-    int meilleure_sol_val = 0;
-
     Result meilleur_resultat = null;
 
     for (int i = 0; i < max_essais; i++){
       Result r = climb(matrix);
-      if (r.valeur > meilleure_sol_val || meilleur_resultat == null){
+      if (r.valeur > meilleur_resultat.valeur || meilleur_resultat == null){
         meilleur_resultat = r;
       }
     }
@@ -155,11 +208,54 @@ public class SteepestHillClimbing {
     return meilleur_resultat;
   }
 
+  /**
+   * Construit une solution aléatoire
+   * @param size dimension de la solution 
+   * @return
+   */
   public boolean[] sol_aleatoire(int size){
     boolean[] res = new boolean[size];
     for (int i = 0; i < size; i++){
       res[i] = rand.nextInt(2) > 0;
     }
     return res;
+  }
+
+  /**
+   * Construit une matrice avec les données contenus dans un fichier.  
+   * 
+   * Les données sont lues sous forme de champs séparés par des espaces, sans newline. 
+   * - un champ pour la dimension de la matrice
+   * - un champ p ? je sais pas
+   * - un champ pour chaque case de la matrice
+   * @param path
+   * @return
+   * @throws IOException
+   * @throws InvalidInputFileException
+   */
+  public int[][] read_solution(Path path) throws IOException, InvalidInputFileException {
+    BufferedReader reader = Files.newBufferedReader(path);
+    String data = reader.readLine(); //une seule ligne
+    String[] fields = data.split(" ");
+
+    if (fields.length < 3){
+      throw new InvalidInputFileException("Le fichier d'entrée doit contenir au moins 3 champs");
+    }
+
+    int size = Integer.valueOf(fields[0]);
+    if (fields.length != (size * size) + 2){
+      throw new InvalidInputFileException("Le fichier d'entrée doit contenir au moins 3 champs");
+    }
+
+    //int p = Integer.valueOf(fields[1]);
+
+    int[][] matrix = new int[size][size];
+    for (int i = 0; i < size; i++){
+      for (int j = 0; j < size; j++){
+        matrix[i][j] = Integer.valueOf(fields[i * size + j]);
+      }
+    }
+
+    return matrix;
   }
 }
